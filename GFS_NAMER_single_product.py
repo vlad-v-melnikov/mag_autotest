addf85c91e78512003b8917fcc3f6ba93853b02b
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-10-14 08:33:53 -0400

    Refactoring names and methods, for better readability

--- GFS_NAMER_single_product.py
+++ GFS_NAMER_single_product.py
@@ -20,78 +20,81 @@ def clear_screenshots():
 
 class MagTestTester(TestParent):
 
+    what_fors = ['test', 'prod']
+    sites = [
+        "https://magtest.ncep.noaa.gov",
+        "https://mag.ncep.noaa.gov",
+    ]
+
     def setUp(self) -> None:
         super().setUp()
         self.settings = Settings()
+        clear_screenshots()
 
-    def _set_cycle_ids(self) -> None:
+    def set_cycle_ids(self) -> None:
         if 'cycle' not in self.settings.links:
             self.settings.links['cycle'] = \
                 self.driver.find_element_by_xpath("//a[contains(@class, 'cycle_link')]").get_attribute('id')
 
-    def _set_product_ids(self) -> None:
+    def set_product_ids(self) -> None:
         if 'products' not in self.settings.links:
             self.settings.links['products'] = \
                 [elem.get_attribute('id') for elem in
                  self.driver.find_elements_by_xpath("//a[contains(@class, 'params_link')]")]
 
+    def set_hour_ids(self, product) -> None:
+        elements = self.driver.find_elements_by_xpath("//a[contains(@id, 'fhr_id_')]")
+        sample = random.sample(range(len(elements)), self.settings.SAMPLE_SIZE)
+        self.settings.links[product] = [elements[i].get_attribute('id') for i in sample]
+
     @retry((NoSuchElementException, StaleElementReferenceException), tries=3, delay=1)
     def click_hour(self, hour):
         self.driver.find_element_by_id(hour).click()
 
-    def test_one_product(self, ty, product) -> None:
+    def test_one_hour(self, hour, what_for, product) -> None:
+        self.click_hour(hour)
+        time.sleep(1)  # let the image load
+        screenshot_region = self.settings.SCREENSHOT_REGION
+        region = screenshot_region
+        pyautogui.screenshot('screenshots/' +
+                             what_for + '_' +
+                             product + '_' +
+                             hour + '.png', region=region)
+        self.driver.find_element_by_class_name('nav_button').click()
+
+    def test_one_product(self, what_for, product) -> None:
         self.driver.find_element_by_id(product).click()
         time.sleep(1)
         if product not in self.settings.links:
-            elements = self.driver.find_elements_by_xpath("//a[contains(@id, 'fhr_id_')]")
-            print(product, str(len(elements)))
-            sample = random.sample(range(len(elements)), self.settings.SAMPLE_SIZE)
-            self.settings.links[product] = [elements[i].get_attribute('id') for i in sample]
-            print(self.settings.links[product])
+            self.set_hour_ids(product)
 
         for hour in self.settings.links[product]:
-            self.click_hour(hour)
-            time.sleep(1)  # let the image load
-            screenshot_region = self.settings.SCREENSHOT_REGION
-            region = screenshot_region
-            pyautogui.screenshot('screenshots/' +
-                                 ty + '_' +
-                                 product + '_' +
-                                 hour + '.png', region=region)
-            self.driver.find_element_by_class_name('nav_button').click()
+            self.test_one_hour(hour, what_for, product)
+
+    def setup_page(self) -> None:
+        self.driver.find_element_by_link_text(self.settings.links['section']).click()
+        self.driver.find_element_by_link_text(self.settings.links['model']).click()
+        self.driver.find_element_by_link_text(self.settings.links['area']).click()
 
     def test_GFS_NAMER_single_product(self) -> None:
-        types = ['test', 'prod']
-        sites = [
-            "https://magtest.ncep.noaa.gov",
-            "https://mag.ncep.noaa.gov",
-        ]
-
-        clear_screenshots()
-
-        for ty, site in zip(types, sites):
+        for what_for, site in zip(self.what_fors, self.sites):
             self.driver.get(site)
+            self.setup_page()
 
-            # setting up the page
-            self.driver.find_element_by_link_text(self.settings.links['section']).click()
-            self.driver.find_element_by_link_text(self.settings.links['model']).click()
-            self.driver.find_element_by_link_text(self.settings.links['area']).click()
-
-            if ty == 'test':
-                self._set_cycle_ids()
-                self._set_product_ids()
+            if what_for == 'test':
+                self.set_cycle_ids()
+                self.set_product_ids()
 
             self.driver.find_element_by_id(self.settings.links['cycle']).click()
 
-            # going over products
             for product in self.settings.links['products']:
-                self.test_one_product(ty, product)
+                self.test_one_product(what_for, product)
+
 
 if __name__ == "__main__":
     unittest.main()
 
 # To Do:
-# 1) refactor to run two drivers in parallel
 # 2) So that we have many test cases as in compare.
 # 3) Retry load for test
 # 4) test and prod drivers in a dictionary
