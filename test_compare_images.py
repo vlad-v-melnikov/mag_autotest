803744729a975218ccc26becf8e6802e032cc9aa
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-10-21 12:51:36 -0400

    Changed subtest to show fails there is diff

--- test_compare_images.py
+++ test_compare_images.py
@@ -22,18 +22,16 @@ class TestCompareImages(unittest.TestCase):
     def test_screens(self):
         screens = zip(glob.glob('screenshots/prod_*.png'), glob.glob('screenshots/test_*.png'))
         for prod, test in screens:
-            with self.subTest():
-                try:
-                    img_prod = self.find_frame(Image.open(prod).convert('RGB'))
-                    img_test = self.find_frame(Image.open(test).convert('RGB'))
-                    diff = ImageChops.difference(img_prod, img_test)
-                    self.assertFalse(bool(diff.getbbox()), f"{test} does not match {prod}")
-                    logging.info(f'{test} MATCHES {prod}')
-                except AssertionError as e:
-                    identifier = prod[(prod.find('_') + 1):prod.find('.png')]
-                    print(str(e))
-                    logging.info(f'{test} DOES NOT match {prod}')
-                    diff.save('screenshots/diff_' + identifier + '.png')
+            img_prod = self.find_frame(Image.open(prod).convert('RGB'))
+            img_test = self.find_frame(Image.open(test).convert('RGB'))
+            diff = ImageChops.difference(img_prod, img_test)
+            try:
+                with self.subTest(f"{test} does not match {prod}"):
+                    self.assertFalse(bool(diff.getbbox()))
+            except AssertionError:
+                identifier = prod[(prod.find('_') + 1):prod.find('.png')]
+                logging.info(f'{test} DOES NOT match {prod}')
+                diff.save('screenshots/diff_' + identifier + '.png')
 
     def find_frame(self, orig_image) -> Image:
         def get_right(x, y):
