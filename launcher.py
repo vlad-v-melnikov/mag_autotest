07e708023df50407fe65e02fc35e0ee568b62fa1
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-10-30 14:21:57 -0400

    Wrapper into a separate file

--- launcher.py
+++ launcher.py
@@ -11,6 +11,7 @@ from datetime import datetime
 
 # internal
 from settings import Settings
+from wrapper import Wrapper
 from gfs_like import GfsLike
 from sref_cluster import SREFCluster
 from panels import Panels
@@ -21,59 +22,6 @@ CLASS_MAP = {
         'SREF-CLUSTER': SREFCluster,
         'STORM-TRACKS': StormTracks,
     }
-
-def clear_screenshots():
-    files = glob.glob('./screenshots/*.png')
-    for f in files:
-        os.unlink(f)
-
-
-def log_config():
-    now = datetime.now()
-    log_time = now.strftime("%Y%m%d%H%M%S")
-    logging.basicConfig(filename=f'logs\screenshot_maker_{log_time}.log', format='%(asctime)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-
-
-class Wrapper:
-    handles = {}
-    driver = {
-        'Firefox': webdriver.Firefox,
-        'Chrome': webdriver.Chrome,
-    }
-
-    def __init__(self):
-        self.settings = Settings()
-        log_config()
-        clear_screenshots()
-
-        self.driver = self.driver[self.settings.driver]()
-        self.driver.set_page_load_timeout(5)
-        self.driver.maximize_window()
-
-        try:
-            self.open_test_site()
-            self.open_prod_site()
-        except TimeoutException as e:
-            logging.error(f"Exception {type(e)} was thrown while trying to open TEST or PROD site")
-
-        if not os.path.isdir('./screenshots'):
-            os.mkdir('./screenshots')
-
-    @retry(TimeoutException, tries=5, delay=1)
-    def open_test_site(self):
-        self.driver.get(self.settings.sites['test'])
-        self.handles['test'] = self.driver.window_handles[0]
-
-    @retry(TimeoutException, tries=5, delay=1)
-    def open_prod_site(self):
-        self.driver.execute_script(f"window.open('{self.settings.sites['prod']}', 'new window')")
-        self.handles['prod'] = self.driver.window_handles[1]
-
-    def tear_down(self):
-        for handle in self.handles.values():
-            self.driver.switch_to.window(handle)
-            self.driver.close()
 
 
 def screenshots_main():
@@ -89,8 +37,8 @@ def screenshots_main():
 
     single_model.make_now()
 
-    print("\nScreenshots taken")
     wrapper.tear_down()
+    print("\nScreenshots taken")
 
 def cycle_match_checker_main():
     pass
