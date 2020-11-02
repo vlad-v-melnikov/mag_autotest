b64395439ab2a7d8b0cf1d3f1bd5fa4a06c6feb7
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-11-02 16:59:37 -0500

    - Added demo for JSON.

---
+++ demos/settings_demo.py
@@ -0,0 +1,28 @@
+import json
+import os
+from pprint import pprint
+
+
+class Settings:
+    def __init__(self):
+
+        with open('settings_demo.json') as json_file:
+            self.settings = json.load(json_file)
+
+        self.sites = self.settings['sites']
+        self.driver = self.settings['driver']
+        self.plan = self.settings['plan']
+        self.compare = self.settings['compare']
+
+
+def main():
+    settings = Settings()
+
+    pprint(settings.sites)
+    pprint(settings.driver)
+    pprint(settings.compare)
+    pprint(settings.plan)
+
+
+if __name__ == "__main__":
+    main()
