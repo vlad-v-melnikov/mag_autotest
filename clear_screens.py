5f76d75063bb368be07182ee669fe0fc156baaa4
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-11-04 11:00:59 -0500

    - Added optional -m/--model parameter to clear_screens.py

--- clear_screens.py
+++ clear_screens.py
@@ -1,13 +1,25 @@
 from glob import glob
 import os
+import argparse
 
 
 def clear_screenshots():
-    files = glob(f'./screenshots/*.png')
+    model = parse_arguments()
+    files = glob(f'./screenshots/*{model}*.png') if model else glob(f'./screenshots/*.png')
     for f in files:
         os.unlink(f)
     if len(files) > 0:
-        print(f"Deleted {len(files)} files. Screenshot folder is now empty.")
+        print(f"Deleted {len(files)} files.")
+    else:
+        print("No files to delete.")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model',
+                        help="Model name which screenshots should be removed")
+    args = parser.parse_args()
+    return args.model
 
 
 if __name__ == '__main__':
