11f5096f95324bd4ff8c2ed2b6db930332923d82
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-12-18 16:56:04 -0500

    Changes for server API: - check_today - finished - compare_images - done:  -- report diff failure To do:  -- all the rest

--- modules/jirainterface.py
+++ modules/jirainterface.py
@@ -3,78 +3,49 @@ import time
 import requests
 import json
 from datetime import datetime
-from modules.settings_jira import SettingsJira
+# from modules.settings_jira import SettingsJira
+from settings_jira import SettingsJira
 import sys
-
-TODAY_TESTCASES = {
-    'GFS': 'MT-T3',
-    'GEFS-SPAG': 'MT-T4',
-    'NAEFS': 'MT-T5',
-    'NAM': 'MT-T6',
-    'NAM-HIRES': 'MT-T7',
-    'FIREWX': 'MT-T8',
-    'RAP': 'MT-T9',
-    'HRRR': 'MT-T10',
-    'HRW-NMMB': 'MT-T11',
-    'HRW-ARW': 'MT-T12',
-    'HRW-ARW2': 'MT-T13',
-    'SREF': 'MT-T14',
-    'HREF': 'MT-T15',
-    'NBM': 'MT-T16',
-    'SREF-CLUSTER': 'MT-T17',
-    'WW3': 'MT-T18',
-    'ESTOFS': 'MT-T19',
-    'ICE-DRIFT': 'MT-T20',
-    'STORM-TRACKS': 'MT-T21',
-    'UAIR': 'MT-T22',
-    'SKEWT': 'MT-T23',
-    'RTMA': 'MT-T24',
-    'TROP': 'MT-T25',
-    'GFS-SND': 'MT-T26',
-    'NAM-SND': 'MT-T27'
-}
-
+from pprint import pprint
 
 class JiraInterface:
     def __init__(self, environment):
         self.settings = SettingsJira(environment)
 
     def send_report_check_today(self, result, test_case_key, comment, start_time):
-        url = "https://api.adaptavist.io/tm4j/v2/testexecutions"
+        url = f"https://nco-jira.ncep.noaa.gov/rest/atm/1.0/testrun/{self.settings.check_today['cycle_key']}/testcase/{test_case_key}/testresult"
         payload = {
-            "projectKey": self.settings.project_key,
-            "testCycleKey": self.settings.check_today['cycle_key'],
-            "testCaseKey": test_case_key,
-            "statusName": result,
-            "environmentName": self.settings.environment,
+            "status": result,
+            "environment": self.settings.environment,
             "actualEndDate": get_now_datetime_utc(),
             "executionTime": (time.time() - start_time) * 1000,
-            "testScriptResults": [
+            "scriptResults": [
                 {
-                    "statusName": result,
-                    "actualEndDate": get_now_datetime_utc(),
-                    "actualResult": comment
+                    "index": 0,
+                    "status": result,
+                    "comment": comment
                 }
-            ],
+            ]
         }
 
         with open(self.settings.token_file) as file:
             token = file.read()
         headers = {
             'Accept': 'application/json',
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': f'Basic {token}',
+            'Cookie': 'JSESSIONID=B6668DF5DA8A0D8B5825AEDD27823258; atlassian.xsrf.token=BZYC-SHM8-8S0W-Z5IJ_1161eb542607d58ae1ad29b0f67da98b39e40fb4_lin'
         }
         requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
     def create_testcase_for_diff(self):
-        url = "https://api.adaptavist.io/tm4j/v2/testcases"
+        url = "https://nco-jira.ncep.noaa.gov/rest/atm/1.0/testcase"
         payload = {
             "projectKey": self.settings.project_key,
             "name": f"Image Diff {get_now_datetime()}",
-            "priorityName": "Normal",
-            "statusName": "Approved",
-            "folderId": self.settings.compare['folder_id']
+            "priority": "Normal",
+            "status": "Approved",
+            "folder": self.settings.compare['folder']
         }
 
         with open(self.settings.token_file) as file:
@@ -82,13 +53,14 @@ class JiraInterface:
 
         headers = {
             'Accept': 'application/json',
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
+            'Authorization': f'Basic {token}',
+            'Content-Type': 'application/json',
+            'Cookie': 'JSESSIONID=B6668DF5DA8A0D8B5825AEDD27823258; atlassian.xsrf.token=BZYC-SHM8-8S0W-Z5IJ_1161eb542607d58ae1ad29b0f67da98b39e40fb4_lin'
         }
 
         result = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-        if "errorCode" not in result.json():
-            return result.json()["key"]
+        if result.ok:
+            return result.json()['key']
         return False
 
     def add_testcase_steps_for_images(self, test_case, images):
@@ -151,57 +123,47 @@ class JiraInterface:
 
     def report_diff_failure(self, test_case, comment):
         # step creation
-        url = f"https://api.adaptavist.io/tm4j/v2/testcases/{test_case}/teststeps"
+        url = f"https://nco-jira.ncep.noaa.gov/rest/atm/1.0/testcase/{test_case}"
         payload = {
-            "mode": "OVERWRITE",
-            "items": []
-        }
-
-        payload["items"].append(
-            {
-                "inline": {
-                    "description": f"Test availability of images for diff",
-                    "expectedResult": "Images are available for diff"
-                }
+            "testScript": {
+                "type": "STEP_BY_STEP",
+                "steps": [
+                    {
+                        "description": "Test availability of images for diff",
+                        "expectedResult": "Images are available for diff"
+                    }
+                ]
             }
-        )
-
-        with open(self.settings.token_file) as file:
-            token = file.read()
-        headers = {
-            'Accept': 'application/json',
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
         }
 
-        requests.request("POST", url, headers=headers, data=json.dumps(payload))
+        headers = get_headers(self.get_token())
+        requests.request("PUT", url, headers=headers, data=json.dumps(payload))
 
         # execution
-        url = "https://api.adaptavist.io/tm4j/v2/testexecutions"
+        url = f"https://nco-jira.ncep.noaa.gov/rest/atm/1.0/testrun/{self.settings.compare['cycle_key']}/testcase/{test_case}/testresult"
         test_script_results = [
             {
-                "statusName": "Fail",
-                "actualEndDate": get_now_datetime_utc(),
-                "actualResult": comment
+                "index": 0,
+                "status": "Fail",
+                "comment": comment
             }
         ]
         payload = {
-            "projectKey": self.settings.project_key,
-            "testCycleKey": self.settings.compare['cycle_key'],
-            "testCaseKey": f"{test_case}",
-            "statusName": "Fail",
-            "testScriptResults": test_script_results,
-            "environmentName": self.settings.environment,
+            "status": "Fail",
+            "scriptResults": test_script_results,
+            "environment": self.settings.environment,
             "actualEndDate": get_now_datetime_utc()
         }
 
-        headers = {
-            'Accept': 'application/json',
-            'Authorization': f'{token}',
-            'Content-Type': 'application/json'
-        }
+        headers = get_headers(self.get_token())
+        response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+        print(response.json())
 
-        requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
+    def get_token(self):
+        with open(self.settings.token_file) as file:
+            token = file.read()
+        return token
 
 
 def get_now_datetime_utc():
@@ -212,9 +174,15 @@ def get_now_datetime():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
+def get_headers(token):
+    return {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Basic {token}',
+            'Cookie': 'JSESSIONID=B6668DF5DA8A0D8B5825AEDD27823258; atlassian.xsrf.token=BZYC-SHM8-8S0W-Z5IJ_1161eb542607d58ae1ad29b0f67da98b39e40fb4_lin'
+        }
+
+
 if __name__ == '__main__':
     jira = JiraInterface('Firefox')
-    test_case = jira.create_testcase_for_diff()
-    if not test_case:
-        sys.exit(0)
-    jira.report_diff_failure(test_case, 'Did not find any images')
+    jira.report_diff_failure('MAG-T29', 'No images for diff found')
--- modules/settings_jira.py
+++ modules/settings_jira.py
@@ -15,30 +15,32 @@ class SettingsJira:
         self.check_today = self.settings['check_today']
         self.TODAY_TESTCASES = self.settings['TODAY_TESTCASES']
 
-        if environment in self.get_environments():
+        if environment in self.get_environments(self.settings['project_key']):
             self.environment = environment
         else:
             self.environment = 'Unknown'
 
-    def get_environments(self):
-        url = "https://api.adaptavist.io/tm4j/v2/environments"
+    def get_environments(self, project_key):
+        url = f"https://nco-jira.ncep.noaa.gov/rest/atm/1.0/environments?projectKey={project_key}"
         with open(self.token_file) as file:
             token = file.read()
         headers = {
             'Accept': 'application/json',
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Basic {token}',
+            'Cookie': 'JSESSIONID=B6668DF5DA8A0D8B5825AEDD27823258; '
+                      'atlassian.xsrf.token=BZYC-SHM8-8S0W-Z5IJ_1161eb542607d58ae1ad29b0f67da98b39e40fb4_lin'
         }
 
         response = requests.request("GET", url, headers=headers).json()
         environments = []
-        for val in response['values']:
+        for val in response:
             environments.append(val['name'])
 
         return environments
 
 
 def main():
-    settings = SettingsJira('IE')
+    settings = SettingsJira('Firefox')
     print(settings.project_key, settings.token_file, settings.environment, sep='\n')
     pprint(settings.compare)
     pprint(settings.check_today)
